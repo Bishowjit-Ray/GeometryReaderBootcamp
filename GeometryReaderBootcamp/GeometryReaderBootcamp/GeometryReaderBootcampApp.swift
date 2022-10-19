47e90d159d44d2176dc82277c08fab95497da0b8
Bishowjit-Ray <82141518+Bishowjit-Ray@users.noreply.github.com>
2022-10-19 15:46:18 +0600

    Add files via upload

---
+++ GeometryReaderBootcamp/GeometryReaderBootcamp/GeometryReaderBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderBootcampApp.swift
+//  GeometryReaderBootcamp
+//
+//  Created by Bishowjit Ray on 19/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
